3598747afd55d691872cdb7e4a80debba7ee7a52
IN-MP-029 <sen.95@hotmail.fr>
2019-04-24 17:02:40 +0200

    Modification de la classe adresse

--- src/com/infotel/metier/Adresse.java
+++ src/com/infotel/metier/Adresse.java
@@ -9,7 +9,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.PreRemove;
-
+// ma classe adresse
 @Entity
 public class Adresse {
 	@Id
